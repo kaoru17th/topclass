d6f39ffb484b6c20f55c02d0cdc00b2637717f83
warscovich <warscovich@gmail.com>
2013-09-08 23:45:14 -0500

    Partial Commit de lo que falta

--- app/controllers/new_semester_controller.rb
+++ app/controllers/new_semester_controller.rb
@@ -1,8 +1,9 @@
 class NewSemesterController < ApplicationController
 	def index
-		if @program_semester == nil then
-			@program_semester= ProgramSemester.first(20);
-		end
+		#if @program_semester == nil then
+			#@program_semester= ProgramSemester.first(20);
+			@program_semester= ProgramSemester.all;
+		#end
 
 		#@program_semester=Program_semester.find(params[:id])
 	end
