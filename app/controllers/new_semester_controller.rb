e5c30696b7189c7af3bdbb63cb1ab70acdd04b9a
warscovich <warscovich@gmail.com>
2013-09-17 22:47:33 -0500

    colores

--- app/controllers/new_semester_controller.rb
+++ app/controllers/new_semester_controller.rb
@@ -19,6 +19,9 @@ class NewSemesterController < ApplicationController
 
 		#Variable hash para contabilizar las personas que se quedan sin clase 
 		@missing_from_subject=Hash.new
+		
+		#Color
+		@missing_from_subject_color=Hash.new
 
 		#Todas las materias deben tener un total
 		@pre_subjects.each do |pre_subject|
@@ -33,16 +36,20 @@ class NewSemesterController < ApplicationController
 			#validación de reglas 
 			#REGLAS
 
-			#Curso lleno! Alarma
-			if pre_subject.subject.quota <= @subjects_hash[tmp_subject_id] then
+		#Curso lleno! Alarma
+		if pre_subject.subject.quota <= @subjects_hash[tmp_subject_id] then
 				#ALARMA
-				if @missing_from_subject[tmp_subject_id_name] ==nil then
-					@missing_from_subject[tmp_subject_id_name] = 0
-				end
-				@missing_from_subject[tmp_subject_id_name] += 1
-			else
-				@subjects_hash[tmp_subject_id] += 1
+			if @missing_from_subject[tmp_subject_id_name] ==nil then
+				@missing_from_subject[tmp_subject_id_name] = 0
 			end
+			
+			@missing_from_subject[tmp_subject_id_name] += 1
+			#Color
+			@missing_from_subject_color[tmp_subject_id] ="border span5 subjectFull"
+		else
+			@subjects_hash[tmp_subject_id] += 1
+			@missing_from_subject_color[tmp_subject_id] ="border span5"
+		end
 
 		end
 	end
